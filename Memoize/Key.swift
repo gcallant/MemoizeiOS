8c4d3afa83c084a21eb21970288e87511ea00005
Grant Callant <grant@grantcallant.com>
2019-05-31 17:16:51 -0700

    Now converts key into consumable format for reality

--- Memoize/Key.swift
+++ Memoize/Key.swift
@@ -8,52 +8,77 @@ import Security
 
 class Key
 {
-   private var publicKey: SecKey? = nil
-   
-   static func setupKeys(_ user: User)
-   {
-      print("Generating key")
-      let publicKey = generateKey(user)
-      print("Key Generated")
-      self.sendKeyToServer(publicKey)
-   }
-   
-   private class func sendKeyToServer(_ publicKey: SecKey?)
-   {
-      //Code to Send Key to server
-   }
-   
-   static func generateKey(_ user: User) -> SecKey?
-   {
-      let tag                       = "\(user.name).\(user.phone).\(user.email)".data(using: .utf8)!
-      let type                      = kSecAttrKeyTypeECSECPrimeRandom
-      var error:      Unmanaged<CFError>?
-      let access                    =
-              SecAccessControlCreateWithFlags(kCFAllocatorDefault,
-                                              kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
-                                              .biometryAny,
-                                              &error)!
-      let attributes: [String: Any] =
-              [
-                 kSecAttrKeyType as String: type,
-                 kSecAttrKeySizeInBits as String: 256,
-                 kSecAttrTokenID as String: kSecAttrTokenIDSecureEnclave,
-                 kSecPrivateKeyAttrs as String:
-                 [
-                    kSecAttrIsPermanent as String: true,
-                    kSecAttrApplicationTag as String: tag,
-                    kSecAttrAccessControl as String: access
-                 ]
-              ]
-      
-      
-      guard let privateKey = SecKeyCreateRandomKey(attributes as CFDictionary, &error)
-      else
-      {
-         print(error!.takeRetainedValue())
-         exit(-1)
-      }
-      
-      return SecKeyCopyPublicKey(privateKey)
-   }
+    private var publicKey: SecKey? = nil
+    
+    static func setupKeys(_ user: User)
+    {
+        print("Generating key")
+        let publicKey = generateKey(user)
+        print("Key Generated")
+        self.sendKeyToServer(publicKey)
+    }
+    
+    private class func sendKeyToServer(_ publicKey: SecKey?)
+    {
+        let address = getServerAddress()
+        if let digestibleKey = convertKey(publicKey)
+        {
+            
+        }
+    }
+    
+    private class func convertKey(_ key: SecKey?) -> String?
+    {
+        let keyData = SecKeyCopyExternalRepresentation(key!, nil)! as Data
+        let keyType       = kSecAttrKeyTypeECSECPrimeRandom
+        let keySize       = 256
+        let exportManager = CryptoExportImportManager()
+        if let exportablePEMKey = exportManager.exportPublicKeyToPEM(keyData, keyType: keyType as String, keySize: keySize)
+        {
+            return exportablePEMKey
+        }
+        
+        return nil
+    }
+    
+    private static func getServerAddress() -> String
+    {
+        let infoPlist = Bundle.main.infoDictionary
+        let address   = infoPlist!["ServerAddress"] as! String
+        return address
+    }
+    
+    static func generateKey(_ user: User) -> SecKey?
+    {
+        let tag                       = "\(user.name).\(user.phone).\(user.email).key".data(using: .utf8)!
+        let type                      = kSecAttrKeyTypeECSECPrimeRandom
+        var error:      Unmanaged<CFError>?
+        let access                    =
+            SecAccessControlCreateWithFlags(kCFAllocatorDefault,
+                                            kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
+                                            .biometryAny,
+                                            &error)!
+        let attributes: [String: Any] =
+            [
+                kSecAttrKeyType as String: type,
+                kSecAttrKeySizeInBits as String: 256,
+                kSecAttrTokenID as String: kSecAttrTokenIDSecureEnclave,
+                kSecPrivateKeyAttrs as String:
+                    [
+                        kSecAttrIsPermanent as String: true,
+                        kSecAttrApplicationTag as String: tag,
+                        kSecAttrAccessControl as String: access
+                ]
+        ]
+        
+        
+        guard let privateKey = SecKeyCreateRandomKey(attributes as CFDictionary, &error)
+            else
+        {
+            print(error!.takeRetainedValue())
+            exit(-1)
+        }
+        
+        return SecKeyCopyPublicKey(privateKey)
+    }
 }
